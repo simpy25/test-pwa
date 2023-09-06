ea61fc7f4df3485e167f956277baff62fbfc985e
Aleksey <aleksey.o@it-dimension.com>
2023-09-06 14:05:09 +0300

    first

---
+++ app/app.config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.
+  module('phonecatApp').
+  config(['$routeProvider',
+    function config($routeProvider) {
+      $routeProvider.
+        when('/phones', {
+          template: '<phone-list></phone-list>'
+        }).
+        when('/phones/:phoneId', {
+          template: '<phone-detail></phone-detail>'
+        }).
+        otherwise('/phones');
+    }
+  ]);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+  })
+}
